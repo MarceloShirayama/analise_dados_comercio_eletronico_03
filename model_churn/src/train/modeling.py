839aa9768d0eba395fe67a2bf7d79c755f478403
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2020-06-15 16:28:46 -0300

    a famigerada ABT parte 3 00:28hs no Twitch.tv - alterado o modeling.py para salvar o modelo de machine learning

--- model_churn/src/train/modeling.py
+++ model_churn/src/train/modeling.py
@@ -4,6 +4,7 @@ import argparse
 import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
+from sklearn import tree
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--date_init", "-i", help="Data referencia para inicio da ABT")
@@ -16,27 +17,50 @@ TRAIN_DIR = os.path.dirname(os.path.abspath(__file__))
 SRC_DIR = os.path.dirname(TRAIN_DIR)
 BASE_DIR = os.path.dirname(SRC_DIR)
 DATA_DIR = os.path.join(BASE_DIR, 'data')
+MODELS_DIR = os.path.join(BASE_DIR, 'models')
 
 OUT_BASE_DIR = os.path.dirname(BASE_DIR)
 OUT_DATA_DIR = os.path.join(OUT_BASE_DIR, "upload_olist", "data")
 DB_PATH = os.path.join(OUT_DATA_DIR, "olist.db")
 
-# # Conexão com o banco de dados
-# print("\n Abrindo conexão com o banco de dados ...")
-# conn = connect_db('sqlite', path=DB_PATH)
-# print("OK! Conexão com o banco de dados aberta.\n")
-
+# Extração da base de dados
+print("\nExtraindo base de dados")
+# Extração de base dados csv
 if args.file_type == 'csv':
     table_name = 'tb_abt_{date_init}_{date_end}.csv'\
         .format(date_init=args.date_init.replace('-',''),
                 date_end=args.date_end.replace('-',''))
     df = pd.read_csv(os.path.join(DATA_DIR, table_name))
 
-if args.file_type == 'sqlite':
+# Extração de base dados do Banco de Dados
+elif args.file_type == 'sqlite':
     conn = connect_db('sqlite', path=DB_PATH)
     table_name = 'tb_abt_{date_init}_{date_end}'\
         .format(date_init=args.date_init.replace('-',''),
                 date_end=args.date_end.replace('-',''))
     df = pd.read_sql_table(table_name, conn)
+print("OK! Término da extração")
 
-print(df.head())
+# Ajuste do modelo de machine learning
+print("\nAjuste do modelo ...")
+features = df.columns[3:-2]
+target = 'flag_churn'
+
+X = df[features]
+y = df[target]
+
+clf = tree.DecisionTreeClassifier(max_depth=8)
+clf.fit(X, y)
+print("OK!")
+
+# Váriaveis mais importantes para o modelo tomar decisão
+print("\nVáriaveis mais importantes para o modelo tomar decisão")
+feature_importance = pd.Series(clf.feature_importances_, \
+    index=features).sort_values()
+print(feature_importance)
+
+# Salvando o modelo
+print("\nSalvando o modelo ...")
+model = pd.Series([features, clf], index=['features', 'model'])
+model.to_pickle(os.path.join(MODELS_DIR, 'model.pkl'))
+print("OK!")
